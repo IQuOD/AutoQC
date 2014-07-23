50c02a94f7d6c368bacca0bc8885407bd56b1e39
arnaldorusso <arnaldorusso@gmail.com>
2014-07-23 14:46:49 -0300

    tests changed before #5

--- tests/AutoQC_test.py
+++ tests/AutoQC_test.py
@@ -1,65 +1,60 @@
 #!/usr/bin/env python
 
 import numpy as np
-import unittest
 
 dummy = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 
-class TestSequence(unittest.TestCase):
+def by_2():
+    ll = []
+    for i in dummy:
+        if i % 2 == 0:
+            ll.append(1)
+        else:
+            ll.append(0)
 
-    def test_by_2(self):
-        ll = []
-        setUp = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
-        for i in dummy:
-            if i % 2 == 0:
-                ll.append(1)
-            else:
-                ll.append(0)
+    assert [1, 0, 1, 0, 1, 0, 1, 0, 1, 0] == ll
 
-        self.assertEqual(setUp, ll)
-        return self.defaultTestResult()
 
-    def test_greater_five(self):
-        ll = []
-        setUp = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1]
-        for i in dummy:
-            if i > 5:
-                ll.append(1)
-            else:
-                ll.append(0)
+def greater_five():
+    ll = []
+    for i in dummy:
+        if i > 5:
+            ll.append(1)
+        else:
+            ll.append(0)
 
-        self.assertEqual(setUp, ll)
+    assert [0, 0, 0, 0, 0, 0, 1, 1, 1, 1] == ll
 
-    def test_by_3(self):
-        ll = []
-        setUp = [0, 1, 0, 0, 1, 0, 0, 1, 0, 0]
-        for i in dummy:
-            if i % 3 == 1:
-                ll.append(1)
-            else:
-                ll.append(0)
 
-        self.assertEqual(setUp, ll)
+def by_3():
+    ll = []
+    for i in dummy:
+        if i % 3 == 1:
+            ll.append(1)
+        else:
+            ll.append(0)
 
-    def test_equal_seven(self):
-        ll = []
-        setUp = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
-        for i in dummy:
-            if i == 7:
-                ll.append(1)
-            else:
-                ll.append(0)
+    assert [0, 1, 0, 0, 1, 0, 0, 1, 0, 0] == ll
 
-        self.assertEqual(setUp, ll)
 
-    def test_less_zero(self):
-        ll = []
-        setUp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        for i in dummy:
-            if i < 0:
-                ll.append(1)
-            else:
-                ll.append(0)
+def equal_seven():
+    ll = []
+    for i in dummy:
+        if i == 7:
+            ll.append(1)
+        else:
+            ll.append(0)
 
-        self.assertEqual(setUp, ll)
+    assert [0, 0, 0, 0, 0, 0, 0, 1, 0, 0] == ll
+
+
+def less_zero():
+    ll = []
+    for i in dummy:
+        if i < 0:
+            ll.append(1)
+        else:
+            ll.append(0)
+
+    assert [0, 0, 0, 0, 0, 0, 0, 0, 0] == ll
