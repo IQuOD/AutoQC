9e1a5cb111143c52cdccea6ea85104fc93ab15c8
arnaldorusso <arnaldorusso@gmail.com>
2014-07-22 22:48:27 -0300

    tests by Mozilla Sprint Challenge #1 - testing pipeline #2

--- tests/AutoQC_test.py
+++ tests/AutoQC_test.py
@@ -4,13 +4,57 @@ import numpy as np
 
 dummy = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+
 def by_2():
     ll = []
     for i in dummy:
-        if i%2 == 0:
+        if i % 2 == 0:
             ll.append(1)
         else:
             ll.append(0)
 
     assert [1, 0, 1, 0, 1, 0, 1, 0, 1, 0] == ll
 
+
+def greater_five():
+    ll = []
+    for i in dummy:
+        if i > 5:
+            ll.append(1)
+        else:
+            ll.append(0)
+
+    assert [0, 0, 0, 0, 0, 0, 1, 1, 1, 1] == ll
+
+
+def by_3():
+    ll = []
+    for i in dummy:
+        if i % 3 == 1:
+            ll.append(1)
+        else:
+            ll.append(0)
+
+    assert [0, 1, 0, 0, 1, 0, 0, 1, 0, 0] == ll
+
+
+def equal_seven():
+    ll = []
+    for i in dummy:
+        if i == 7:
+            ll.append(1)
+        else:
+            ll.append(0)
+
+    assert [0, 0, 0, 0, 0, 0, 0, 1, 0, 0] == ll
+
+
+def less_zero():
+    ll = []
+    for i in dummy:
+        if i < 0:
+            ll.append(1)
+        else:
+            ll.append(0)
+
+    assert [0, 0, 0, 0, 0, 0, 0, 0, 0] == ll
