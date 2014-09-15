4c3f837cdbd2f4dba43a7a69cf431c2c9c169237
simon.good <s-good@users.noreply.github.com>
2014-09-15 12:52:42 +0100

    Added some real test data and adapted code to show how to use the data

--- AutoQC.py
+++ AutoQC.py
@@ -9,27 +9,62 @@ filenames = json.loads(open('datafiles.json').read())
 # Read all profiles from the files and store in a list.
 profiles = []
 for filename in filenames:
-    with open(filename) as f:
-        profiles += [wod.WodProfile(f)]
+    with open(filename) as f:        
+        profiles.append(wod.WodProfile(f))
         while profiles[-1].is_last_profile_in_file(f) == False:
-            profiles += [wod.WodProfile(f)]
+            profiles.append(wod.WodProfile(f))
+
+# In some IQuOD datasets temperature values of 99.9 are special values to 
+# signify not to use the data value. These are flagged here so they are not
+# sent to the quality control programs for testing.
+for profile in profiles:
+    index = profile.var_index()
+    assert index is not None, 'No temperature data in profile %s' % profile.uid()
+    for i in range(profile.n_levels()):
+        if profile.profile_data[i]['variables'][index]['Missing']: 
+            continue
+        if profile.profile_data[i]['variables'][index]['Value'] == 99.9:
+            profile.profile_data[i]['variables'][index]['Missing'] = True
 
 # Placeholder for a section of code that 
 #  - detects all quality control checks in dataio.
 #  - uses ddt to run all quality control checks on all profiles.
-#  - saves the overall result for each in a numpy array.
+#  - saves the overall result for each in a numpy array. The overall result
+#      is simply whether any level in the profile was rejected by the 
+#      quality control.
+# The reference results are the correct results for each level of the
+# profile. We want to know how often the overall result for the quality 
+# control check matches the overall reference result.
+nFailedQC  = 0
+nFailedRef = 0
 for profile in profiles:
     # Run a quality control check on this profile.
     qcResults = EN_range_check.test(profile)
+    
+    # Extract the reference result for each level.
+    qcRefs    = profile.t_level_qc(originator=True) >= 3
 
     # Print out a summary of the results.
-    print('---------------------------------')
+    print('-----------------------------------------')
     print('Profile ID: %i' % profile.uid())
     print('')
-    print('Depth (m) Temp (degC) Result')
+    print('Depth (m) Temp (degC) Result Reference')
     for iLevel, qc in enumerate(qcResults):
-        print('%7.1f    %6.1f     %s' % (profile.z()[iLevel], profile.t()[iLevel], qc))   
-    print('      OVERALL RESULT: %s' % np.any(qc))
-    print('---------------------------------')
+        print('%6s      %5s     %5s   %5s' % (profile.z()[iLevel], 
+                                profile.t()[iLevel], qc, qcRefs[iLevel]))   
+    print('     OVERALL RESULTS: %5s   %5s' % 
+                                (np.any(qcResults), np.ma.any(qcRefs)))
+    print('-----------------------------------------')
 
+    # Collate some statistics.
+    if np.any(qcResults): nFailedQC += 1
+    if np.ma.any(qcRefs): nFailedRef += 1
+
+print('Number of profiles tested was %i' % len(profiles))
+print('Number of profiles that failed the quality control test was %i' %
+                                                                      nFailedQC)
+print('Number of profiles that should have been failed was %i' % nFailedRef)
+
+
+                                                                      
 
