c150e77e508a957daf73be532855b7d9459271c7
BillMills <mills.wj@gmail.com>
2015-05-10 10:54:42 -0700

    validation tests for argo_pressure_increasing_test

--- qcvalidation.py
+++ qcvalidation.py
@@ -1,5 +1,6 @@
 import qctests.Argo_global_range_check
 import qctests.Argo_gradient_test
+import qctests.Argo_pressure_increasing_test
 import qctests.EN_range_check
 import util.testingProfile
 import numpy
@@ -155,6 +156,32 @@ def test_Argo_gradient_test_temperature_floatingPoint():
     qc = qctests.Argo_gradient_test.test(p)
     truth = numpy.zeros(3, dtype=bool)
     assert numpy.array_equal(qc, truth), 'floating point error'
+
+##### Argo_pressure_increasing_test ---------------------------------------------------
+
+def test_Argo_pressure_increasing_test_constantPressure():
+    '''
+    API test should flag only the subsequent levels of constant pressure.
+    '''
+
+    p = util.testingProfile.fakeProfile([2,2,2], [100,100,100]) 
+    qc = qctests.Argo_pressure_increasing_test.test(p)
+    truth = numpy.zeros(3, dtype=bool)
+    truth[1] = True
+    truth[2] = True
+    assert numpy.array_equal(qc, truth), 'must flag only subsequent levels of constant pressure.'    
+
+def test_Argo_pressure_increasing_test_pressureInversion():
+    '''
+    API test should flag only the subsequent levels of constant pressure.
+    '''
+
+    p = util.testingProfile.fakeProfile([2,2,2], [100,200,100]) 
+    qc = qctests.Argo_pressure_increasing_test.test(p)
+    truth = numpy.zeros(3, dtype=bool)
+    truth[1] = True
+    truth[2] = True
+    assert numpy.array_equal(qc, truth), 'must flag all levels corresponding to pressure inversion.' 
 
 ##### EN_range_check ---------------------------------------------------
 
