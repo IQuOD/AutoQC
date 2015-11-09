fe5a1e8ce7eef4859dbde0ac933a86e28cae23da
BillMills <mills.wj@gmail.com>
2015-11-08 17:15:11 -0800

    variable filenames for post-QC step

--- util/splitData.py
+++ util/splitData.py
@@ -4,8 +4,8 @@ import main
 import math
 from wodpy import wod
 
-#filename = '../../AutoQC_raw/quota/quota_all.dat'
-filename = '../data/quota_subset.dat'
+filename = '../../AutoQC_raw/quota/quota_all.dat'
+#filename = '../data/quota_subset.dat'
 n = 30
 
 fid = open(filename)
@@ -34,25 +34,3 @@ while not (fid.read(1) == ''):
         target.close()
         fileNo += 1
         target = open('split-' + str(fileNo) + '.dat', 'w')
-
-# headers = main.extractProfiles([filename]);
-
-# nPerFile = math.ceil(len(headers) / n)
-# fid = open(filename)
-
-# for fileNo in range(n):
-#     target = open('split-' + str(fileNo) + '.dat', 'w')
-#     for i in range(int(nPerFile)):
-#         start = fid.tell()
-#         profile = wod.WodProfile(fid)
-#         end = fid.tell()
-
-#         fid.seek(start)
-#         extract = fid.read(end-start)
-#         target.write(extract)
-#         fid.seek(end)
-#         # test for eof
-#         if fid.read(1) == '':
-#             break
-#         fid.seek(end)
-#     target.close()
