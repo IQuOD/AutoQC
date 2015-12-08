8c547f464a75ca6f8cb136aa2c3aca8770956dc2
s-good <s-good@users.noreply.github.com>
2015-12-08 13:05:11 +0000

    First attempt to implement the CoTeDe QC tests in the AutoQC framework. Anomaly detection is implemented and appears to work. AutoQC output has also been tidied up. A new routine is introduced that checks that requirements such as data files are available for each qc test. This is introduced because not all users may want to install CoTeDe if developing their own code.

--- AutoQC.py
+++ AutoQC.py
@@ -1,10 +1,10 @@
 from wodpy import wod
 import glob, time
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import sys, os, json
 import util.combineTests as combinatorics
-import util.benchmarks as benchmarks
+#import util.benchmarks as benchmarks
 import util.main as main
 
 def run(test, profiles, kwargs):
@@ -104,14 +104,18 @@ def dumpRawResults(testResults, trueResults):
 filenames = main.readInput('datafiles.json')
 profiles = main.extractProfiles(filenames)
 print('{} profiles will be read'.format(len(profiles)))
+print('')
 
 # identify and import tests
 testNames = main.importQC('qctests')
 testNames.sort()
-print('{} quality control checks will be applied:'.format(len(testNames)))
+print('{} quality control checks have been found'.format(len(testNames)))
+testNames = main.checkQCTestRequirements(testNames)
+print('{} quality control checks are able to be run:'.format(len(testNames)))
 for testName in testNames:
-  print(' {}'.format(testName))
+  print('  {}'.format(testName))
   exec('from qctests import ' + testName)
+print('')
 
 # Set up any keyword arguments needed by tests.
 kwargs = {'profiles' : profiles}
@@ -155,7 +159,7 @@ for iprofile, pinfo in enumerate(profiles):
   trueResults.append(truth[0][0])
   trueVerbose.append(truth[1][0])
   # Update user on progress.
-  sys.stdout.write('{:5.1f}% complete\r'.format((iprofile+1)*100.0/len(profiles)))
+  sys.stdout.write('QC of profiles is {:5.1f}% complete\r'.format((iprofile+1)*100.0/len(profiles)))
   sys.stdout.flush()
 # testResults[i][j] now contains a flag indicating the exception raised by test i on profile j
 
@@ -164,10 +168,19 @@ for i in reversed(delete):
   del profiles[i]
 
 # Summary statistics
+print('')
+print('')
 print('Number of profiles tested was %i' % len(profiles))
-for i in range (0, len(testNames)):
-  print('Number of profiles that failed ' + testNames[i] + ' was %i' % np.sum(testResults[i]))
 print('Number of profiles that should have been failed was %i' % np.sum(trueResults))
+print('')
+print('%30s %7s %7s %7s %7s %7s' % ('NAME OF TEST', 'FAILS', 'TPR', 'FPR', 'TNR', 'FNR')) 
+overallResults = np.zeros(len(profiles), dtype=bool)
+for i in range (0, len(testNames)):
+  overallResults = np.logical_or(overallResults, testResults[i])
+  tpr, fpr, fnr, tnr = main.calcRates(testResults[i], trueResults)
+  print('%30s %7i %6.1f%1s %6.1f%1s %6.1f%1s %6.1f%1s' % (testNames[i], np.sum(testResults[i]), tpr, '%', fpr, '%', tnr, '%', fnr, '%'))
+tpr, fpr, fnr, tnr = main.calcRates(overallResults, trueResults)
+print('%30s %7i %6.1f%1s %6.1f%1s %6.1f%1s %6.1f%1s' % ('RESULT OF OR OF ALL:', np.sum(overallResults), tpr, '%', fpr, '%', tnr, '%', fnr, '%'))
 
 # generate a set of logical combinations of tests
 #combos = combinatorics.combineTests(testResults)
@@ -181,4 +194,4 @@ print('Number of profiles that should have been failed was %i' % np.sum(trueResu
 
 #logfile
 #generateLogfile(testVerbose, trueVerbose, profiles, testNames)
-dumpRawResults(testResults, trueResults)
+#dumpRawResults(testResults, trueResults)
