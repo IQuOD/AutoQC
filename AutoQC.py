85e8ed23fb5afa0a71fbaa84a76c54462dce8bed
s-good <s-good@users.noreply.github.com>
2015-12-14 00:14:00 +0000

    Continuing to tidy up output code.

--- AutoQC.py
+++ AutoQC.py
@@ -23,69 +23,7 @@ def run(test, profiles):
     verbose.append(result)
   return [qcResults, verbose]
 
-def generateLogfile(verbose, trueVerbose, profiles, testNames):
-  '''
-  verbose[i][j][k] == result of test i on profile j at depth k
-  trueVerbose[j][k] == true result for profile j at depth k
-  <profiles> == array of profiles per `extractProfiles`
-  <testNames> == array of names returned by `importQC`
-  '''
-  # open a logfile
-  logfile = open('AutoQClog.' + time.strftime("%H%M%S"), 'w')
-
-  # log summary for each profile
-  for i in range (0, len(profiles)): # i counts profiles
-    with open(profiles[i].file_name) as f:
-      f.seek(profiles[i].file_position)
-      profile = wod.WodProfile(f)
-      
-    logfile.write('Profile ID: %i\n' % profiles[i].uid())
-
-    # title row
-    titleRow = 'Depth (m)  Temp (degC)'
-    formatString = '{0[0]:<20}{0[1]:<20}'
-    k = 2
-    for test in testNames:
-        titleRow += '  ' + test
-        formatString += '{0['+str(k)+']:<20}'
-        k += 1
-    titleRow += '  Reference'
-    formatString += '{0['+str(k)+']:<20}'
-    logfile.write(formatString.format(titleRow.split('  ')))
-    logfile.write('\n')
-
-    # row for each depth
-    for j in range (0,len(trueVerbose[i])): # j counts depth
-      formatString = '{0[0]:<20}{0[1]:<20}'
-      row = str(profile.z()[j]) + '  ' + str(profile.t()[j]) + '  '
-      for k in range (0, len(verbose)): # k counts tests
-        row += str(verbose[k][i][j])
-        row += '  '
-        formatString += '{0['+str(2+k)+']:<20}'
-      row += str(trueVerbose[i][j])
-      formatString += '{0['+str(2+len(verbose))+']:<20}'
-      logfile.write(formatString.format(row.split('  ')))
-      logfile.write('\n')
-
-    summaryRow = '  OVERALL RESULTS:  '
-    formatString = '{0[0]:<20}{0[1]:<20}'
-    for k in range (0, len(verbose)):
-      summaryRow += str(np.any(verbose[k][i]))
-      summaryRow += '  '
-      formatString += '{0['+str(2+k)+']:<20}'
-    summaryRow += str(np.any(trueVerbose[i]))
-    formatString += '{0['+str(2+len(verbose))+']:<20}'
-    logfile.write(formatString.format(summaryRow.split('  ')))
-    logfile.write('\n')
-
-    logfile.write('-----------------------------------------\n')
-
-
 def processFile(fName):
-  profiles = main.extractProfiles([fName])
-  print('{} profiles will be read'.format(len(profiles)))
-  print('')
-
   # run each test on each profile, and record its summary & verbose performance
   testResults  = []
   testVerbose  = []
@@ -93,7 +31,6 @@ def processFile(fName):
   trueVerbose  = []
   profileIDs   = []
   firstProfile = True
-  delete       = []
   currentFile  = ''
   f = None
   for iprofile, pinfo in enumerate(profiles):
@@ -139,14 +76,16 @@ testNames = main.checkQCTestRequirements(testNames)
 print('{} quality control checks are able to be run:'.format(len(testNames)))
 for testName in testNames:
   print('  {}'.format(testName))
-print('')
 
 # identify data files and extract profile information into an array - this
 # information is used by some quality control checks; the profile data are
 # read later.
 filenames = main.readInput('datafiles.json')
+profiles  = main.extractProfiles(filenames)
+print('\n{} profiles will be read\n'.format(len(profiles)))
 
 if len(sys.argv)>2:
+  nProcessed = 0
   processFile.parallel = main.parallel_function(processFile, sys.argv[2])
   parallel_result = processFile.parallel(filenames)
   #recombine results
@@ -159,21 +98,8 @@ if len(sys.argv)>2:
       results[itr] += tr
     profileIDs += pr[2]
 
-  # Summary statistics
-  nProfiles = len(truth)
-  print('')
-  print('')
-  print('Number of profiles tested was %i' % nProfiles)
-  print('')
-  print('%30s %7s %7s %7s %7s %7s' % ('NAME OF TEST', 'FAILS', 'TPR', 'FPR', 'TNR', 'FNR')) 
-  overallResults = np.zeros(nProfiles, dtype=bool)
-  for i in range (0, len(testNames)):
-    overallResults = np.logical_or(overallResults, results[i])
-    tpr, fpr, fnr, tnr = main.calcRates(results[i], truth)
-    print('%30s %7i %6.1f%1s %6.1f%1s %6.1f%1s %6.1f%1s' % (testNames[i], np.sum(results[i]), tpr, '%', fpr, '%', tnr, '%', fnr, '%'))
-  tpr, fpr, fnr, tnr = main.calcRates(overallResults, truth)
-  print('%30s %7i %6.1f%1s %6.1f%1s %6.1f%1s %6.1f%1s' % ('RESULT OF OR OF ALL:', np.sum(overallResults), tpr, '%', fpr, '%', tnr, '%', fnr, '%'))
-
+  # Print summary statistics and write output file.
+  main.printSummary(truth, results, testNames)
   main.generateCSV(truth, results, testNames, profileIDs, sys.argv[1])
 else:
   print 'Please add command line arguments to name your output file and set parallelization:'
