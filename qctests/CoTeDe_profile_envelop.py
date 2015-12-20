7887e2453136c4bda2252db7209807d0396f9660
Gui <guilherme@castelao.net>
2015-12-19 20:52:58 -0800

    profile_envelope moved to profile_envelop @ CoTeDe.

--- qctests/CoTeDe_profile_envelop.py
+++ qctests/CoTeDe_profile_envelop.py
@@ -1,10 +1,10 @@
 from cotede_qc.cotede_test import get_qc
 
 def test(p):
-    '''Run the profile_envelope QC from the CoTeDe config.'''
+    '''Run the profile_envelop QC from the CoTeDe config.'''
 
     config   = 'cotede'
-    testname = 'profile_envelope'
+    testname = 'profile_envelop'
 
     return get_qc(p, config, testname)
 
