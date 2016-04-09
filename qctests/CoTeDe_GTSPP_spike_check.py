4b0cf960aa67b805e8cdb0b7a43e7e27e59c0853
Gui <guilherme@castelao.net>
2016-04-08 18:05:39 -0700

    Using CoTeDe's default configs.
    CoTeDe now try to run the minimum ammount of tests required, so it is
    not required anymore to explicitly define setup with a dictionary.

--- qctests/CoTeDe_GTSPP_spike_check.py
+++ qctests/CoTeDe_GTSPP_spike_check.py
@@ -3,7 +3,7 @@ from cotede_qc.cotede_test import get_qc
 def test(p):
     '''Run the spike QC from the CoTeDe GTSPP config.'''
 
-    config   = {'TEMP': {'spike': 2.0}}
+    config   = 'gtspp'
     testname = 'spike'
 
     return get_qc(p, config, testname)
--- qctests/CoTeDe_rate_of_change.py
+++ qctests/CoTeDe_rate_of_change.py
@@ -3,7 +3,7 @@ from cotede_qc.cotede_test import get_qc
 def test(p):
     '''Run the rate_of_change QC from the CoTeDe config.'''
 
-    config   = {'TEMP': {'rate_of_change': 4}}
+    config   = 'cotede'
     testname = 'rate_of_change'
 
     return get_qc(p, config, testname)
