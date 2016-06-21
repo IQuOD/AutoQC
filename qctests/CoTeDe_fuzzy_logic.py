5727849793411e6a3dce40780a64cf5b97a382c7
Simon Good <s-good@users.noreply.github.com>
2016-06-21 23:05:53 +0100

    Update CoTeDe_fuzzy_logic.py
    Fuzzy logic config should be picked up from CoTeDe after this change. Config files in AutoQC should now be superfluous.

--- qctests/CoTeDe_fuzzy_logic.py
+++ qctests/CoTeDe_fuzzy_logic.py
@@ -3,7 +3,7 @@ from cotede_qc.cotede_test import get_qc
 def test(p):
     '''Run the CoTeDe fuzzy logic QC.'''
 
-    config   = 'fuzzy'
+    config   = 'fuzzylogic'
     testname = 'fuzzylogic'
 
     return get_qc(p, config, testname)
