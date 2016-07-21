063e4407d1ed22604a83a246212108b7feb42feb
Bill Mills <mills.wj@gmail.com>
2016-07-21 23:57:20 +0000

    fix for csiro short gradient divide by 0 problems

--- qctests/CSIRO_short_gradient.py
+++ qctests/CSIRO_short_gradient.py
@@ -20,11 +20,13 @@ def test(p):
     
     # initialize qc as a bunch of falses;
     qc = numpy.zeros(len(t), dtype=bool)
-
+    
     for i in range(0, p['n_levels']-1 ):
         if main.dataPresent(('t', 'z'), i, p) and main.dataPresent(('t', 'z'), i+1, p):
             deltaD = (d[i+1] - d[i]) 
             deltaT = (t[i+1] - t[i])
+            if deltaT == 0:
+              continue
             gradshort = deltaD / deltaT 
             if (deltaT > 0.5 and deltaD < 30) or abs(gradshort) < 0.4 or (gradshort > 0 and gradshort < 12.5):
                 if abs(deltaT) > 0.4:
