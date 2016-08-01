0f68b970da2d1eb2b0add25d348e45265470fab6
s-good <s-good@users.noreply.github.com>
2016-08-01 22:16:59 +0100

    Added functionality to use sqlite database plus tidied up setting table name, added usage instructions.

--- AutoQC.py
+++ AutoQC.py
@@ -3,7 +3,16 @@ import glob, time
 import numpy as np
 import sys, os, data.ds
 import util.main as main
-import pandas, psycopg2
+import pandas
+try:
+    import psycopg2 as db
+    dbtype = 'postgres'
+    concom = "dbname='root' user='root'"
+except:
+    import sqlite3 as db
+    concom = 'qcresults.sqlite'
+    dbtype = 'sqlite'
+print('Database type is ' + dbtype)
 from multiprocessing import Pool
 import tempfile
 
@@ -48,7 +57,7 @@ if len(sys.argv)>2:
     '''run all tests on the ith database row'''
   
     # extract profile
-    cur.execute('SELECT * FROM validate WHERE uid = ' + str(uid) )
+    cur.execute('SELECT * FROM ' + sys.argv[1] + ' WHERE uid = ' + str(uid) )
     row = cur.fetchall()
     fProfile = tempfile.TemporaryFile()
     fProfile.write(row[0][0]) # a file-like object containing only the profile from the queried row
@@ -68,13 +77,16 @@ if len(sys.argv)>2:
     for itest, test in enumerate(testNames):
       
       result = run(test, [profile])
-      query = "UPDATE validate SET " + test.lower() + " = " + str(result[0][0]) + " WHERE uid = " + str(profile.uid()) + ";"
+      query = "UPDATE " + sys.argv[1] + " SET " + test.lower() + " = " + str(int(result[0][0])) + " WHERE uid = " + str(profile.uid()) + ";"
       cur.execute(query)
+      if dbtype == 'sqlite':
+          # Seem to need to do this after every update for sqlite database.
+          conn.commit()
       
   # connect to database & fetch list of all uids
-  conn = psycopg2.connect("dbname='root' user='root'")
+  conn = db.connect(concom)
   cur = conn.cursor()
-  cur.execute('SELECT uid FROM validate')
+  cur.execute('SELECT uid FROM ' + sys.argv[1])
   uids = cur.fetchall()
   
   # launch async processes
@@ -88,5 +100,5 @@ if len(sys.argv)>2:
   
 else:
   print 'Please add command line arguments to name your output file and set parallelization:'
-  print 'python AutoQC myFile 4'
-  print 'will result in output written to results-myFile.csv, and will run the calculation parallelized across 4 cores.'
+  print 'python AutoQC databasetable 4'
+  print 'will result in output written to table in the database, and will run the calculation parallelized across 4 cores.'
