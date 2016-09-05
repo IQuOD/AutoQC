5bde72d88dcd085f5833c253b0cd1ed57dd3b363
Bill Mills <mills.wj@gmail.com>
2016-09-05 20:19:49 +0000

    small corrections

--- launchQC.sh
+++ launchQC.sh
@@ -1,11 +1,11 @@
 # bash launchQC.sh <number of profiles> <number of processes>
 
-QUEUE=$(($1 / $2))
+QUEUE=$(($2 / $3))
 
-for i in `seq 0 $(($2 - 2))`
+for i in `seq 0 $(($3 - 2))`
 do
-  echo python AutoQC.py demo $(($i * $QUEUE)) $(($(($i + 1)) * $QUEUE)) &
+  python AutoQC.py $1 $(($i * $QUEUE)) $(($(($i + 1)) * $QUEUE)) &
 done
 
-echo python AutoQC.py demo $(( $(($i + 1)) * $QUEUE)) $1 &
+python AutoQC.py demo $(( $(($i + 1)) * $QUEUE)) $1 &
 
