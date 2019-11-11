983a5733825485c793dcb18628fa2bec97391bdb
BillMills <mills.wj@gmail.com>
2019-11-11 18:15:54 -0500

    flag options for main routine

--- AutoQC.py
+++ AutoQC.py
@@ -1,5 +1,5 @@
 from wodpy import wod
-import pickle, sys, os, calendar, time
+import pickle, sys, os, calendar, time, ast
 import numpy as np
 import util.main as main
 from multiprocessing import Pool
@@ -16,7 +16,7 @@ def run(test, profiles, parameters):
 
   return verbose
 
-def process_row(uid, logdir):
+def process_row(uid, logdir, table='iquod'):
   '''run all tests on the indicated database row'''
 
   # reroute stdout, stderr to separate files for each profile to preserve logs
@@ -38,7 +38,7 @@ def process_row(uid, logdir):
       result = np.zeros(1, dtype=bool)
 
     try:
-      query = "UPDATE " + sys.argv[1] + " SET " + test + "=? WHERE uid=" + str(profile.uid()) + ";"
+      query = "UPDATE " + table + " SET " + test + "=? WHERE uid=" + str(profile.uid()) + ";"
       main.dbinteract(query, [main.pack_array(result)])
     except:
       print('db exception', sys.exc_info())
@@ -48,56 +48,76 @@ def process_row(uid, logdir):
 # main
 ########################################
 
-if len(sys.argv)>2:
+# parse options
+options, remainder = getopt.getopt(sys.argv[1:], 't:d:b:n:h')
+targetdb = 'iquod.db'
+dbtable = 'iquod'
+batchnumber = None
+nperbatch = None
+for opt, arg in options:
+    if opt == '-b':
+        batchnumber = ast.literal_eval(arg)
+    if opt == '-d':
+        dbtable = arg
+    if opt == '-n':
+        cores = ast.literal_eval(arg)
+    if opt == '-p':
+        nperbatch = ast.literal_eval(arg)
+    if opt == '-t':
+        targetdb = arg
+    if opt == '-h':
+        print('usage:')
+        print('-b <batch number to process>')
+        print('-d <db table name to create and write to>')
+        print('-n <number of cores to use>')
+        print('-p <how many profiles to process per batch>')
+        print('-t <name of db file>')
+        print('-h print this help message and quit')
 
-  # Identify and import tests
-  testNames = main.importQC('qctests')
-  testNames.sort()
-  print('{} quality control checks have been found'.format(len(testNames)))
-  testNames = main.checkQCTestRequirements(testNames)
-  print('{} quality control checks are able to be run:'.format(len(testNames)))
-  for testName in testNames:
-    print('  {}'.format(testName))
+# Identify and import tests
+testNames = main.importQC('qctests')
+testNames.sort()
+print('{} quality control checks have been found'.format(len(testNames)))
+testNames = main.checkQCTestRequirements(testNames)
+print('{} quality control checks are able to be run:'.format(len(testNames)))
+for testName in testNames:
+  print('  {}'.format(testName))
 
-  # set up a directory for logging
-  logdir = "autoqc-logs-" + str(calendar.timegm(time.gmtime()))
-  os.makedirs(logdir)
+# set up a directory for logging
+logdir = "autoqc-logs-" + str(calendar.timegm(time.gmtime()))
+os.makedirs(logdir)
 
-  # Parallel processing.
-  print('\nPlease wait while QC is performed\n')
+# Parallel processing.
+print('\nPlease wait while QC is performed\n')
 
-  # set up global parmaeter store
-  parameterStore = {
-    "table": sys.argv[1]
-  }
-  for test in testNames:
-    exec('from qctests import ' + test)
-    try:
-      exec(test + '.loadParameters(parameterStore)')
-    except:
-      print('No parameters to load for', test)
+# set up global parmaeter store
+parameterStore = {
+  "table": dbtable
+}
+for test in testNames:
+  exec('from qctests import ' + test)
+  try:
+    exec(test + '.loadParameters(parameterStore)')
+  except:
+    print('No parameters to load for', test)
 
-  # connect to database & fetch list of all uids
-  query = 'SELECT uid FROM ' + sys.argv[1] + ' ORDER BY uid;'
-  uids = main.dbinteract(query)
+# connect to database & fetch list of all uids
+query = 'SELECT uid FROM ' + dbtable + ' ORDER BY uid;'
+uids = main.dbinteract(query)
 
-  # launch async processes
-  if len(sys.argv) > 4:
-    batchnumber = int(sys.argv[3])
-    nperbatch   = int(sys.argv[4])
-    startindex  = batchnumber*nperbatch
-    endindex    = min((batchnumber+1)*nperbatch,len(uids))
-  else:
-    startindex  = 0
-    endindex    = len(uids)
-  pool = Pool(processes=int(sys.argv[2]))
-  for i in range(startindex, endindex):
-    pool.apply_async(process_row, (uids[i][0], logdir))
-  pool.close()
-  pool.join()
-
+# launch async processes
+if batchnumber is not None and nperbatch is not None:
+  batchnumber = int(batchnumber)
+  nperbatch   = int(nperbatch)
+  startindex  = batchnumber*nperbatch
+  endindex    = min((batchnumber+1)*nperbatch,len(uids))
 else:
-  print('Please add command line arguments to name your output file and set parallelization:')
-  print('python AutoQC <database results table> <number of processes> <batch> <number of processes per batch> [<batchnumber> <number per batch>]')
-  print('will use <database results table> to log QC results in the database, and run the calculation parallelized over <number of processes>. By default all profiles will be processed, but optionally the processing can be done in batches of size <number per batch>.')
+  startindex  = 0
+  endindex    = len(uids)
+pool = Pool(processes=int(cores))
+for i in range(startindex, endindex):
+  pool.apply_async(process_row, (uids[i][0], logdir, dbtable))
+pool.close()
+pool.join()
+
 
